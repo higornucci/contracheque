876182161d350e44020362c998fef66c7ef58b8f
Higor Nucci <higornucci@gmail.com>
2016-10-19 21:03:48 -0300

    grafico com salário

--- app/src/main/java/br/com/higornucci/contracheque/DisplaySalarioActivity.java
+++ app/src/main/java/br/com/higornucci/contracheque/DisplaySalarioActivity.java
@@ -1,10 +1,17 @@
 package br.com.higornucci.contracheque;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.Arrays;
 
 public class DisplaySalarioActivity extends AppCompatActivity {
 
@@ -15,11 +22,18 @@ public class DisplaySalarioActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
+        float salarioBruto = Float.valueOf(message);
 
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
-        layout.addView(textView);
+        PieEntry pieEntry = new PieEntry(salarioBruto, "Salário Líquido");
+        PieDataSet pieDataSet = new PieDataSet(Arrays.asList(pieEntry, new PieEntry(65f, "Imposto")), "Salário");
+        pieDataSet.addColor(Color.GREEN);
+        pieDataSet.addColor(Color.CYAN);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        PieChart pieChart = (PieChart) findViewById(R.id.piechart_salario);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+        pieChart.notifyDataSetChanged();
     }
 }
