b0945abd7a74a1eb3366087066e22cf68865b48d
Higor Nucci <higornucci@gmail.com>
2016-10-24 09:52:32 -0300

    Criando contrato para o vencimento

---
+++ app/src/main/java/br/com/higornucci/contracheque/repositorio/vencimento/VencimentoContract.java
@@ -0,0 +1,26 @@
+package br.com.higornucci.contracheque.repositorio.vencimento;
+
+import android.provider.BaseColumns;
+
+public class VencimentoContract {
+
+    public VencimentoContract() {
+    }
+
+    public static class VencimentoEntry implements BaseColumns {
+        public static final String TABLE_NAME = "vencimento";
+        public static final String COLUMN_DESCRICAO = "descricao";
+        public static final String COLUMN_VALOR = "valor";
+        private static final String PRIMARY_KEY_TYPE = "INTEGER PRIMARY KEY";
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String COMMA_SEP = ",";
+        private static final String SQL_CREATE_ENTRIES =
+                "CREATE TABLE " + VencimentoEntry.TABLE_NAME + " (" +
+                        VencimentoEntry._ID + " " + PRIMARY_KEY_TYPE + COMMA_SEP +
+                        VencimentoEntry.COLUMN_DESCRICAO + TEXT_TYPE + COMMA_SEP +
+                        VencimentoEntry.COLUMN_VALOR + TEXT_TYPE + " )";
+
+        private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + VencimentoEntry.TABLE_NAME;
+    }
+}
