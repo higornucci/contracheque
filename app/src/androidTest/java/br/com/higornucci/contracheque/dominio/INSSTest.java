895a1b92ad96b20e0306bbb68ff11a845a301c82
Higor Nucci <higornucci@gmail.com>
2016-10-25 15:30:54 -0300

    Teste para o calculo do inss com aliquota minima

--- app/src/androidTest/java/br/com/higornucci/contracheque/dominio/INSSTest.java
+++ app/src/androidTest/java/br/com/higornucci/contracheque/dominio/INSSTest.java
@@ -20,4 +20,15 @@ public class INSSTest {
 
         assertThat(valorDescontado, is(equalTo(valorDescontadoEsperado)));
     }
+
+    @Test
+    public void deve_calcular_o_valor_do_inss_com_O_valor_teto_da_aliquota_minima_descontado_do_salario() throws Exception {
+        Real valorDoSalarioBruto = new Real(new BigDecimal(1556.94));
+        Real valorDescontadoEsperado = new Real(new BigDecimal(124.5552));
+
+        CalculadorDeDesconto calculadorDeDesconto = new CalculadorDeINSS(valorDoSalarioBruto);
+        Real valorDescontado = calculadorDeDesconto.calcular();
+
+        assertThat(valorDescontado, is(equalTo(valorDescontadoEsperado)));
+    }
 }
